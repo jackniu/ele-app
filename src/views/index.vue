db70ff4f19c73414f15f971228dafff2ffa01363
jackniu <chuangnzh@qq.com>
2019-04-15 01:25:26 +0800

    Init

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="index">
+    主页
+  </div>
+</template>
+<script>
+export default {
+    name: "index"
+}
+</script>
+
+<style>
+#app {
+    width: 100%;
+    height: 100%;
+    font-size: 14px;
+    background: #f1f1f1;
+}
+</style>
