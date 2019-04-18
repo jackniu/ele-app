38fe0e70b712da7eabef58a5f9b506d3dd534c22
jackniu <chuangnzh@qq.com>
2019-04-18 10:15:20 +0800

    Add Login page

--- src/router.js
+++ src/router.js
@@ -1,17 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from './views/index.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'index',
-      component: Index
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.ele_login ? true : false;
+  if (to.path == '/login') {
+    next();
+  } else {
+    isLogin ? next() : next('/login');
+  }
+})
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">login</div>
+</template>
+<script>
+export default {
+    name: "login"
+}
+</script>
+
+<style>
+.login {
+    width: 100%;
+    height: 100%;
+    padding: 30px;
+    box-sizing: border-box;
+    background: #fff;
+    text-align: center;
+}
+</style>
