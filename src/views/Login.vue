3671bb5d44233ea0cb27afb67290793724316fd0
jackniu <chuangnzh@qq.com>
2019-04-18 14:43:02 +0800

    Add input group component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="login">login</div>
+    <div class="login">
+        <div class="logo">
+            <img src="../assets/logo.jpg" alt="">
+        </div>
+        <InputGroup type="number" v-model="phone" placeholder="手机号"
+         :btnTitle="btnTitle" :disabled="disabled" :error="errors.phone" />
+        <InputGroup type="number" v-model="verifyCode" placeholder="验证码"
+         :error="errors.code" />
+        <!-- 用户服务协议 -->
+        <div class="login_des">
+            <p>新用户登录即自动注册，表示已同意
+                <span>《用户服务协议》</span>
+            </p>
+        </div>
+        <!-- 登录按钮 -->
+        <div class="login_btn">
+            <button>登录</button>
+        </div>
+    </div>
 </template>
 <script>
+import InputGroup from '../components/InputGroup.vue';
 export default {
-    name: "login"
+    name: "login",
+    data() {
+        return {
+            phone: "",
+            verifyCode: "",
+            errors: {},
+            btnTitle: "获取验证码",
+            disabled: false
+        }
+    },
+    components: {
+        InputGroup
+    }
 }
 </script>
 
@@ -14,6 +45,36 @@ export default {
     padding: 30px;
     box-sizing: border-box;
     background: #fff;
+}
+.logo {
     text-align: center;
+}
+.logo img {
+    width: 150px;
+}
+.text_group,
+.login_des,
+.login_btn {
+  margin-top: 20px;
+}
+.login_des {
+  color: #aaa;
+  line-height: 22px;
+}
+.login_des span {
+  color: #4d90fe;
+}
+.login_btn button {
+  width: 100%;
+  height: 40px;
+  background-color: #48ca38;
+  border-radius: 4px;
+  color: white;
+  font-size: 14px;
+  border: none;
+  outline: none;
+}
+.login_btn button[disabled] {
+  background-color: #8bda81;
 }
 </style>
